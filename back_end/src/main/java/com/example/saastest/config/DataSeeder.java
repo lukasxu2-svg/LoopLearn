3099c1a3fdd8efdd00443a4c945089a351331433
Lukas <utvaz@student.kit.edu>
2026-08-04 14:15:19 +0200

    Added new env variables and adjusted initial data seeder

--- back_end/src/main/java/com/example/saastest/config/DataSeeder.java
+++ back_end/src/main/java/com/example/saastest/config/DataSeeder.java
@@ -4,6 +4,7 @@ import com.example.saastest.modules.benutzer.repository.BenutzerRepository;
 import com.example.saastest.modules.plan.entity.Plan;
 import com.example.saastest.modules.plan.enums.PlanType;
 import com.example.saastest.modules.plan.repository.PlanRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,16 @@ import java.util.List;
 
 @Configuration
 public class DataSeeder {
+
+    @Value("${paypal.product.id.free}")
+    private String freePlanId;
+
+    @Value("{paypal.product.id.basic}")
+    private String basicPlanId;
+
+    @Value("{paypal.product.id.premium}")
+    private String premiumPlanId;
+
     @Bean
     CommandLineRunner seedDatabase(
             PlanRepository planRepository,
@@ -24,13 +35,12 @@ public class DataSeeder {
 
             if (planRepository.count() == 0) {
 
-                Plan none = new Plan(PlanType.NONE, new BigDecimal("0.0"));
-                Plan free = new Plan(PlanType.FREE, new BigDecimal("0.0"));
-                Plan basic = new Plan(PlanType.BASIC, new BigDecimal("1.0"));
-                Plan premium = new Plan(PlanType.PREMIUM, new BigDecimal("5.0"));
+                Plan free = new Plan(PlanType.FREE, new BigDecimal("0.0"), freePlanId);
+                Plan basic = new Plan(PlanType.BASIC, new BigDecimal("1.0"), basicPlanId);
+                Plan premium = new Plan(PlanType.PREMIUM, new BigDecimal("5.0"), premiumPlanId);
 
 
-                planRepository.saveAll(List.of(none, free, basic, premium));
+                planRepository.saveAll(List.of(free, basic, premium));
             }
         };
     }
