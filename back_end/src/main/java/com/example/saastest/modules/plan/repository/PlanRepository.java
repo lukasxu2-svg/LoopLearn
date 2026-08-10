bcc20d25a41cf3476e2946110fb030a2876924c4
Lukas <utvaz@student.kit.edu>
2026-08-10 10:05:56 +0200

    Added findByPlanId to plan repo

--- back_end/src/main/java/com/example/saastest/modules/plan/repository/PlanRepository.java
+++ back_end/src/main/java/com/example/saastest/modules/plan/repository/PlanRepository.java
@@ -3,5 +3,8 @@ package com.example.saastest.modules.plan.repository;
 import com.example.saastest.modules.plan.entity.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlanRepository extends JpaRepository<Plan, Long> {
+    Optional<Plan> findByPlanId(String planId);
 }
