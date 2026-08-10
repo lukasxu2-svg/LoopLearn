c3fb68813cb74ef6458e481d175dceee1df93a7d
Lukas <utvaz@student.kit.edu>
2026-08-10 09:55:56 +0200

    Changed wrongtype of NameDto

--- back_end/src/main/java/com/example/saastest/modules/payment/paypal/plans/dto/NameDto.java
+++ back_end/src/main/java/com/example/saastest/modules/payment/paypal/plans/dto/NameDto.java
@@ -3,6 +3,6 @@ package com.example.saastest.modules.payment.paypal.plans.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record NameDto(
-                @JsonProperty("given_name") String givenName,
-                StrictMath surname) {
+        @JsonProperty("given_name") String givenName,
+        String surname) {
 }
