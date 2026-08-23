385f6e77e817d1f041e0558a8db692ba9eb13c94
Lukas <utvaz@student.kit.edu>
2026-08-23 10:52:58 +0200

    Added different constructor for getCurrentSubscriptionResponseBody

--- back_end/src/main/java/com/example/saastest/modules/subscription/dto/response/getCurrentSubscriptionResponseBody.java
+++ back_end/src/main/java/com/example/saastest/modules/subscription/dto/response/getCurrentSubscriptionResponseBody.java
@@ -1,7 +1,9 @@
 package com.example.saastest.modules.subscription.dto.response;
 
 import com.example.saastest.modules.payment.paypal.subscriptions.dto.enums.SubscriptionStatusDto;
+import com.example.saastest.modules.plan.entity.Plan;
 import com.example.saastest.modules.plan.enums.PlanType;
+import com.example.saastest.modules.subscription.entity.Subscription;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -12,6 +14,18 @@ public record getCurrentSubscriptionResponseBody(
         BigDecimal cost,
         Instant periodStart,
         Instant periodEnd,
-        boolean cancelled
+        boolean cancelled,
+        getCurrentSubscriptionResponseBody nextSubscription
 ) {
+    public getCurrentSubscriptionResponseBody(Subscription subscription, Plan plan, getCurrentSubscriptionResponseBody nextSubscription) {
+        this(
+                subscription.getSubStatus(),
+                plan.getPlanType(),
+                plan.getMonthlyPrice(),
+                subscription.getPeriodStart(),
+                subscription.getPeriodEnd(),
+                subscription.isCanceled(),
+                nextSubscription
+        );
+    }
 }
