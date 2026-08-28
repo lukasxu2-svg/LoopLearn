864d70836f0ee87f45bc40f26219da8b12a6525c
Lukas <utvaz@student.kit.edu>
2026-08-28 13:40:03 +0200

    Adjusted webhook bahaviour on subscription activation and added case to handle false payment

--- back_end/src/main/java/com/example/saastest/modules/payment/paypal/webhooks/service/PaypalWebhookService.java
+++ back_end/src/main/java/com/example/saastest/modules/payment/paypal/webhooks/service/PaypalWebhookService.java
@@ -1,10 +1,14 @@
 package com.example.saastest.modules.payment.paypal.webhooks.service;
 
+import com.example.saastest.modules.benutzer.entity.Benutzer;
 import com.example.saastest.modules.payment.paypal.auth.service.PaypalTokenService;
 import com.example.saastest.modules.payment.paypal.subscriptions.dto.enums.SubscriptionStatusDto;
+import com.example.saastest.modules.payment.paypal.subscriptions.dto.request.CancelSubscriptionRequestBody;
+import com.example.saastest.modules.payment.paypal.subscriptions.service.PayPalSubscriptionService;
 import com.example.saastest.modules.payment.paypal.webhooks.dto.WebhookVerificationRequestBody;
 import com.example.saastest.modules.payment.service.BaseService;
 import com.example.saastest.modules.plan.entity.Plan;
+import com.example.saastest.modules.plan.enums.PlanType;
 import com.example.saastest.modules.plan.repository.PlanRepository;
 import com.example.saastest.modules.subscription.entity.Subscription;
 import com.example.saastest.modules.subscription.repository.SubscriptionRepository;
@@ -15,6 +19,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import tools.jackson.databind.JsonNode;
 
 import java.time.Instant;
+import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.Optional;
 
@@ -23,11 +28,14 @@ import java.util.Optional;
 public class PaypalWebhookService extends BaseService {
     private final SubscriptionRepository subscriptionRepository;
     private final PlanRepository planRepository;
+    private final PayPalSubscriptionService payPalService;
 
-    public PaypalWebhookService(PaypalTokenService tokenService, WebClient webClient, SubscriptionRepository subscriptionRepository, PlanRepository planRepository) {
+
+    public PaypalWebhookService(PaypalTokenService tokenService, WebClient webClient, SubscriptionRepository subscriptionRepository, PlanRepository planRepository, PayPalSubscriptionService payPalService) {
         super(tokenService, webClient);
         this.subscriptionRepository = subscriptionRepository;
         this.planRepository = planRepository;
+        this.payPalService = payPalService;
     }
 
     public void receiveWebhook(JsonNode webEvent, WebhookVerificationRequestBody requestBody) {
@@ -43,19 +51,27 @@ public class PaypalWebhookService extends BaseService {
         switch (eventType) {
 
             case "BILLING.SUBSCRIPTION.ACTIVATED": {
-                //Check if old subscription is active
                 String subscriptionId = event.get("resource").get("id").asString();
-                Subscription subscription = subscriptionRepository.findBySubscriptionId(subscriptionId).orElseThrow(() -> new RuntimeException("Subscription not found"));
+                Optional<Subscription> activeSubscription = subscriptionRepository.findBySubStatus(SubscriptionStatusDto.ACTIVE);
+                Optional<Subscription> pendingSubscription = subscriptionRepository.findBySubStatus(SubscriptionStatusDto.APPROVAL_PENDING);
 
-                Optional<Subscription> activeSub = subscriptionRepository.findBySubStatus(SubscriptionStatusDto.ACTIVE);
-
-                if (activeSub.isPresent()) {
-                    activeSub.get().setSubStatus(SubscriptionStatusDto.CANCELLED);
-                    subscriptionRepository.save(activeSub.get());
+                if ((activeSubscription.isPresent() && !activeSubscription.get().getPlanType().equals(PlanType.FREE)) ||
+                        (pendingSubscription.isPresent() && !pendingSubscription.get().getSubscriptionId().equals(subscriptionId))) {
+                    payPalService.paypalCancelSubscription(
+                            subscriptionId,
+                            new CancelSubscriptionRequestBody("Active/Pending subscription already exists")
+                    );
+                    throw new IllegalStateException("Active/Pending subscription already exists");
                 }
 
-                subscription.setSubStatus(SubscriptionStatusDto.ACTIVE);
-                subscriptionRepository.save(subscription);
+                if (activeSubscription.isPresent() && activeSubscription.get().getPlanType().equals(PlanType.FREE)) {
+                    activeSubscription.get().setSubStatus(SubscriptionStatusDto.CANCELLED);
+                    subscriptionRepository.save(activeSubscription.get());
+                }
+
+
+                pendingSubscription.get().setSubStatus(SubscriptionStatusDto.ACTIVE);
+                subscriptionRepository.save(pendingSubscription.get());
                 break;
             }
 
@@ -72,6 +88,7 @@ public class PaypalWebhookService extends BaseService {
                 //If updated subscription has same plan type as current, cancel next subscription
                 if (newPlan.getPlanType().equals(currentSubscription.getPlanType())) {
                     nextSubscription.setCanceled();
+                    nextSubscription.setSubStatus(SubscriptionStatusDto.CANCELLED);
                     subscriptionRepository.save(nextSubscription);
 
                     currentSubscription.setNextSubscription(null);
@@ -86,11 +103,13 @@ public class PaypalWebhookService extends BaseService {
                     nextSubscription.setPlanType(newPlan.getPlanType());
                     subscriptionRepository.save(nextSubscription);
                 } else {
+                    //Creates next subscription if that entry was empty before
                     Instant newPeriodEnd = currentSubscription.getPeriodEnd().atZone(ZoneOffset.UTC).plusMonths(1).toInstant();
 
                     //Create new subscription entry
                     Subscription newNextSubscription = new Subscription(
                             currentSubscription.getSubscriptionId(),
+                            null,
                             currentSubscription.getBenutzer(),
                             planId,
                             newPlan.getPlanType(),
@@ -109,9 +128,29 @@ public class PaypalWebhookService extends BaseService {
 
             case "BILLING.SUBSCRIPTION.CANCELLED": {
                 String subscriptionId = event.get("resource").get("id").asString();
-                Subscription subscription = subscriptionRepository.findBySubscriptionId(subscriptionId).orElseThrow(() -> new RuntimeException("Subscription not found"));
+                Subscription subscription = subscriptionRepository.findBySubscriptionIdAndSubStatus(subscriptionId, SubscriptionStatusDto.ACTIVE)
+                        .orElseThrow(() -> new RuntimeException("Subscription not found"));
+
+                if (subscription.getNextSubscription() != null) {
+                    subscription.getNextSubscription().setCanceled();
+                    subscription.getNextSubscription().setSubStatus(SubscriptionStatusDto.CANCELLED);
+                    subscriptionRepository.save(subscription.getNextSubscription());
+                }
+
                 subscription.setCanceled();
+                subscription.setNextSubscription(null);
                 subscriptionRepository.save(subscription);
+                break;
+            }
+
+            case "PAYMENT.SALE.COMPLETED": {
+                String subscriptionId = event.get("resource").get("billing_agreement_id").asString();
+                String captureId = event.get("resource").get("id").asString();
+
+                Optional<Subscription> subscription = subscriptionRepository.findBySubscriptionId(subscriptionId);
+                if (subscription.isEmpty()) {
+                    payPalService.paypalRefund(captureId);
+                }
                 break;
             }
 
