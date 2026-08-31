c09fa20a5c499829f2302646b045e69f197dcd79
Lukas <utvaz@student.kit.edu>
2026-08-31 12:48:54 +0200

    Moved url endpoint for api.js into an env file

--- front_end/src/services/api.js
+++ front_end/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-import {data} from "react-router-dom";
 
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
     baseURL: API_BASE_URL,
